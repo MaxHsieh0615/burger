21f029bf549848b1446a3af50693e24a09229375
MaxH <MaxH@Maxs-MacBook-Pro.local>
2019-01-14 21:56:57 -0800

    update jaws

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,24 @@
 // Dependencies
 var mysql = require("mysql");
 
+
 // Creates mySQL connection using Mysql, the empty string in the third argument spot is our password.
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "baby22732864",
-  database: "burgers_db"
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "baby22732864",
+    database: "burgers_db"
+  });
+};
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("id: " + connection.threadId)
 });
 
-connection.connect(function(err){
-    if (err) throw err;
-    console.log("id: " + connection.threadId)
-})
-
+connection.connect();
 module.exports = connection;
